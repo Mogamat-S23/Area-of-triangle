80db73aa3a778c605a45528640f62cf81dd611b0
Mogamat-S23 <saleemariefdien@gmail.com>
2022-06-20 13:41:03 +0200

    added css

--- script/code.js
+++ script/code.js
@@ -8,5 +8,28 @@ c.addEventListener('click', () => {
     let b = base.value;
     let answer = eval (((1/2) * b * h ));
 
-    document.getElementById('output').innerHTML = answer ;
+    document.getElementById('output').innerHTML = answer  ;
+    output.value = answer + 'cm²'
 })
+
+let btnClear = document.getElementById('clear');
+
+btnClear.addEventListener('click', () => {
+    let base = document.getElementById('b')
+    base.value = ''
+
+    let height = document.getElementById('h')
+    height.value = ''
+
+    let output = document.getElementById('output')
+    output.value=''
+
+    document.getElementById('b').innerHTML = ''
+    document.getElementById('h').innerHTML = ''
+    document.getElementById('output').innerHTML=''
+    
+
+
+
+
+});
